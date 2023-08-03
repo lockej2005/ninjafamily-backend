48c5970dee61807d344d02ea43de58a920fde9fa
lockej2005 <josh@intelligentinstallations.com.au>
2023-08-03 13:55:45 +1000

    removed port

--- api.js
+++ api.js
@@ -7,8 +7,7 @@ const RedisStore = require('connect-redis')(session);
 
 // Redis client configuration
 const client = redis.createClient({
-  host: 'promisestat.redis.cache.windows.net',
-  port: 6380,
+  url: 'redis://promisestat.redis.cache.windows.net:6380',
   password: 'DTUTjdPPtjjFoVbrq0XLG2fqvgiCIpA8yAzCaED90XE',
   tls: {
       rejectUnauthorized: false
