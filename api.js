b6ec0643dbedd64c59f6629051637cf4d688f161
lockej2005 <josh@intelligentinstallations.com.au>
2023-08-04 00:34:13 +1000

    new uri

--- api.js
+++ api.js
@@ -7,12 +7,12 @@ const RedisStore = require('connect-redis')(session);
 
 // Redis client configuration
 const client = redis.createClient({
-  url: 'promisestat.redis.cache.windows.net',
-  password: 'DTUTjdPPtjjFoVbrq0XLG2fqvgiCIpA8yAzCaED90XE',
+  url: 'rediss://:DTUTjdPPtjjFoVbrq0XLG2fqvgiCIpA8yAzCaED90XE@promisestat.redis.cache.windows.net:6380',
   tls: {
       rejectUnauthorized: false
   }
 });
+
 
 client.on('error', (err) => {
     console.log('Redis error: ', err);
