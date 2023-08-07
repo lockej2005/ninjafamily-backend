605fc0be6abce53152db6b783836a5f6fc5b9e66
lockej2005 <josh@intelligentinstallations.com.au>
2023-08-07 11:24:45 +1000

    add git ignore

--- database.js
+++ database.js
@@ -1,106 +1,115 @@
 const sql = require('mssql');
+require('dotenv').config();
+const dbUser = process.env.DB_USERNAME;
+const dbPass = process.env.DB_PASSWORD;
+const dbUrl = process.env.DB_URL;
+
 
 const config = {
-  user: 'lockej2005',
-  password: 'T0mat0P0tat0',
-  server: 'promisestatserver.database.windows.net',
-  database: 'promisedb',
-  options: {
-    encrypt: true,
-  },
+    user: dbUser,
+    password: dbPass,
+    server: dbUrl,
+    database: 'promisedb',
+    options: {
+        encrypt: true,
+    },
+    connectionTimeout: 5 * 60 * 1000,
+    requestTimeout: 5 * 60 * 1000,
 };
+
 const connectionPool = new sql.ConnectionPool(config).connect()
-  .then(pool => {
-    console.log('Connected to MSSQL')
-    return pool
-  })
-  .catch(err => console.log('Database Connection Failed! ', err));
-const getAllPromises = async (senusername) => {
-  try {
-    const pool = await connectionPool;
-    const result = await pool.request()
-      .input('senusername', sql.NVarChar, senusername)
-      .query('SELECT * FROM promises WHERE senusername = @senusername');
-    return result.recordset;
-  } catch (err) {
-    console.log('Error running the query!', err);
-  }
-};
+    .then(pool => {
+        console.log('Connected to MSSQL');
+        return pool;
+    })
+    .catch(err => console.log('Database Connection Failed! ', err));
+
 const getUser = async (username, password) => {
-  try {
-    const pool = await connectionPool;
-    const result = await pool.request()
-      .input('username', sql.NVarChar, username)
-      .query('SELECT * FROM users WHERE username = @username');
-    const user = result.recordset[0];
+    console.log(`Fetching user with username: ${username} from DB...`);
+    try {
+        const pool = await connectionPool;
+        const result = await pool.request()
+            .input('username', sql.NVarChar, username)
+            .query('SELECT * FROM users WHERE username = @username');
+        const user = result.recordset[0];
 
-    console.log("Fetched user:", user);  // Add this line to check the user fetched from the database
+        console.log("Fetched user:", user);
 
-    // Check if user exists and the password is correct
-    if (user && password === user.password) {
-      // Never return the password or other sensitive fields in the response
-      delete user.password;
-      return user;
-    } else {
-      return null;
+        if (user && password === user.password) {
+            delete user.password;
+            return user;
+        } else {
+            return null;
+        }
+    } catch (err) {
+        console.log('Error running the query!', err);
+        return null;
     }
-  } catch (err) {
-    console.log('Error running the query!', err);
-    return null;
-  }
 };
 
-
-
+const getAllPromises = async (senusername) => {
+    console.log(`Fetching all promises for user: ${senusername} from DB...`);
+    try {
+        const pool = await connectionPool;
+        const result = await pool.request()
+            .input('senusername', sql.NVarChar, senusername)
+            .query('SELECT * FROM promises WHERE senusername = @senusername');
+        return result.recordset;
+    } catch (err) {
+        console.log('Error running the query!', err);
+    }
+};
 
 const addPromise = async (promise, status, recusername, senusername, sentAt) => {
-  try {
-    const pool = await connectionPool;
-    const result = await pool.request()
-      .input('promise', sql.NVarChar, promise)
-      .input('status', sql.NVarChar, status)
-      .input('recusername', sql.NVarChar, recusername)
-      .input('senusername', sql.NVarChar, senusername)
-      .input('sentAt', sql.DateTime, new Date(sentAt))
-      .query('INSERT INTO promises (promise, status, recusername, senusername, sentAt) VALUES (@promise, @status, @recusername, @senusername, @sentAt)');
-    return result.rowsAffected;
-  } catch (err) {
-    console.log('Error running the query!', err);
-  }
+    console.log('Inserting promise into DB...');
+    try {
+        const pool = await connectionPool;
+        const result = await pool.request()
+            .input('promise', sql.NVarChar, promise)
+            .input('status', sql.NVarChar, status)
+            .input('recusername', sql.NVarChar, recusername)
+            .input('senusername', sql.NVarChar, senusername)
+            .input('sentAt', sql.DateTime, new Date(sentAt))
+            .query('INSERT INTO promises (promise, status, recusername, senusername, sentAt) VALUES (@promise, @status, @recusername, @senusername, @sentAt)');
+        return result.rowsAffected;
+    } catch (err) {
+        console.log('Error running the query!', err);
+    }
 };
+
 const addUser = async (username, password, email) => {
-  try {
-    const pool = await connectionPool;
-    const result = await pool.request()
-      .input('username', sql.NVarChar, username)
-      .input('password', sql.NVarChar, password) // remember to hash password before storing it
-      .input('email', sql.NVarChar, email)
-      .input('promise_score', sql.Int, 0) // initialize with 0
-      .query('INSERT INTO users (username, password, email, promise_score) VALUES (@username, @password, @email, @promise_score)');
-    return result.rowsAffected;
-  } catch (err) {
-    console.log('Error running the query!', err);
-  }
+    console.log(`Adding new user with username: ${username} to DB...`);
+    try {
+        const pool = await connectionPool;
+        const result = await pool.request()
+            .input('username', sql.NVarChar, username)
+            .input('password', sql.NVarChar, password)
+            .input('email', sql.NVarChar, email)
+            .query('INSERT INTO users (username, password, email) VALUES (@username, @password, @email)');
+        return result.rowsAffected;
+    } catch (err) {
+        console.log('Error running the query!', err);
+    }
 };
 
 const updatePromiseStatus = async (id, status) => {
-  try {
-    const pool = await connectionPool;
-    const result = await pool.request()
-      .input('id', sql.Int, id)
-      .input('status', sql.NVarChar, status)
-      .query('UPDATE promises SET status = @status WHERE id = @id');
-    return result.rowsAffected;
-  } catch (err) {
-    console.log('Error running the query!', err);
-  }
+    console.log(`Updating promise with id: ${id} in DB...`);
+    try {
+        const pool = await connectionPool;
+        const result = await pool.request()
+            .input('id', sql.Int, id)
+            .input('status', sql.NVarChar, status)
+            .query('UPDATE promises SET status = @status WHERE id = @id');
+        return result.rowsAffected;
+    } catch (err) {
+        console.log('Error running the query!', err);
+    }
 };
 
-
 module.exports = {
-  addPromise,
-  updatePromiseStatus,
-  getAllPromises,
-  addUser,
-  getUser, // don't forget to export it
+    getUser,
+    getAllPromises,
+    addPromise,
+    addUser,
+    updatePromiseStatus
 };
--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-  const express = require('express');
-  const cors = require('cors');
-  const app = express();
-  const apiRouter = require('./api');
+const express = require('express');
+const cors = require('cors');
+const api = require('./api');
 
-  app.use(express.json());
+const app = express();
+const port = 3000;
 
-  const allowedOrigins = ['http://localhost:3000', 'https://promisestat.azurewebsites.net'];
+console.log('Initializing Server...');
 
-  app.use(cors({
+app.use(cors({
     origin: function(origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
+        console.log('Handling CORS check for origin:', origin);
+        callback(null, true);
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT'],
-  }));
+}));
 
-  app.use('/api', apiRouter);
+app.use(express.json());
+app.use('/api', api);
 
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
-  });
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
 
