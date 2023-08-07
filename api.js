a352d4f7c57047c2abed882630128448511383a9
lockej2005 <josh@intelligentinstallations.com.au>
2023-08-07 11:24:37 +1000

    cache finally working

--- api.js
+++ api.js
@@ -4,82 +4,110 @@ const db = require('./database');
 const session = require('express-session');
 const redis = require('redis');
 const RedisStore = require('connect-redis')(session);
+require('dotenv').config();
+
+const redisUrl = process.env.REDIS_URL;
+const redisKey = process.env.REDIS_ACCESS_KEY;
+
+
+
 
 // Redis client configuration
 const client = redis.createClient({
-  url: 'redis://promisestat.redis.cache.windows.net',
-  password: 'DTUTjdPPtjjFoVbrq0XLG2fqvgiCIpA8yAzCaED90XE',
-  tls: {
-      rejectUnauthorized: false
-  }
+    legacyMode: true,
+    url: redisUrl, 
+    password: redisKey,
+    tls: {
+        rejectUnauthorized: false,
+    },
+    retry_strategy: function (options) {
+        if (options.attempt > 10) {
+            return undefined;
+        }
+        return options.attempt * 150;
+    },
 });
 
 client.on('error', (err) => {
-    console.log('Redis error: ', err);
+    console.log('Redis Client Error: ', err);
 });
 
-// Middleware to check if a user is authenticated
+console.log('Connecting to Redis...');
+client.connect()
+    .then(() => console.log("Connected to Redis"))
+    .catch(err => console.log('Redis connect error: ', err));
+
 function checkAuthenticated(req, res, next) {
+    console.log('Entering checkAuthenticated middleware...');
     if (req.session && req.session.username) {
+        console.log('User authenticated');
         next();
     } else {
-        // User is not authenticated, respond with 401 status
         res.status(401).json({ message: 'User not logged in.' });
     }
 }
-
-// Setup session middleware
+console.log('api.js test 1')
 router.use(session({
-  store: new RedisStore({ client: client }),
-  secret: 'Your_Secret_Key',
-  resave: false,
-  saveUninitialized: false
+    store: new RedisStore({ client: client }),
+    secret: redisKey,
+    resave: false,
+    saveUninitialized: false
 }));
 
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  const user = await db.getUser(username, password);
-  if(user) {
-    req.session.username = username;
-    res.status(200).json({ message: 'User logged in successfully.' });
-  } else {
-    res.status(401).json({ message: 'Invalid username or password.' });
-  }
+    console.log('Handling /login POST request...');
+    const { username, password } = req.body;
+
+    if (typeof username !== 'string' || typeof password !== 'string') {
+        res.status(400).json({ message: 'Invalid data.' });
+        return;
+    }
+    const user = await db.getUser(username, password);
+    if (user) {
+        req.session.username = username;
+        console.log(req.session);
+        res.status(200).json({ message: 'User logged in successfully.' });
+    } else {
+        res.status(401).json({ message: 'Invalid username or password.' });
+    }
 });
 
 router.get('/get-promises', checkAuthenticated, async (req, res) => {
-  const senusername = req.session.username;
-  try {
-    const promises = await db.getAllPromises(senusername); // pass senusername as parameter
-    return res.status(200).json(promises);
-  } catch (err) {
-    return res.status(500).json({ error: 'Error retrieving promises.' });
-  }
+    console.log('Handling /get-promises GET request...');
+    const senusername = req.session.username;
+    try {
+        const promises = await db.getAllPromises(senusername);
+        return res.status(200).json(promises);
+    } catch (err) {
+        return res.status(500).json({ error: 'Error retrieving promises.' });
+    }
 });
 
 router.post('/add-promise', checkAuthenticated, async (req, res) => {
-  const { promise, recusername, sentAt } = req.body;
-  const senusername = req.session.username;
-  const status = 'active';
+    console.log('Handling /add-promise POST request...');
+    const { promise, recusername, sentAt } = req.body;
+    const senusername = req.session.username;
+    const status = 'active';
 
-  try {
-    await db.addPromise(promise, status, recusername, senusername, sentAt);
-    return res.status(200).json({ message: 'Promise added successfully.' });
-  } catch (err) {
-    return res.status(500).json({ error: 'Error adding the promise.' });
-  }
+    try {
+        await db.addPromise(promise, status, recusername, senusername, sentAt);
+        return res.status(200).json({ message: 'Promise added successfully.' });
+    } catch (err) {
+        return res.status(500).json({ error: 'Error adding the promise.' });
+    }
 });
 
 router.put('/update-promise/:id', checkAuthenticated, async (req, res) => {
-  const { id } = req.params;
-  const { status } = req.body;
+    console.log(`Handling /update-promise/${req.params.id} PUT request...`);
+    const { id } = req.params;
+    const { status } = req.body;
 
-  try {
-    await db.updatePromiseStatus(id, status);
-    return res.status(200).json({ message: 'Promise status updated successfully.' });
-  } catch (err) {
-    return res.status(500).json({ error: 'Error updating the promise status.' });
-  }
+    try {
+        await db.updatePromiseStatus(id, status);
+        return res.status(200).json({ message: 'Promise status updated successfully.' });
+    } catch (err) {
+        return res.status(500).json({ error: 'Error updating the promise status.' });
+    }
 });
 
 module.exports = router;
